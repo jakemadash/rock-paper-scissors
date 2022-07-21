8c0a6535501ba2846cd11dff7dab80056adf83b0
Jake Ashton <jakemaddenashton@gmail.com>
2022-07-20 18:39:32 -0700

    Write getPlayerChoice function

--- javascript.js
+++ javascript.js
@@ -1,15 +1,29 @@
-function getComputerChoice () {
+function getComputerChoice() {
    let choice = Math.floor(Math.random() * 3);
     console.log(choice);
+
    if (choice === 0) {
-    choice = 'rock';
+    choice = 'Rock';
    }
    else if (choice === 1) {
-    choice = 'paper';
+    choice = 'Paper';
    }
-   else choice = 'scissors';
+   else choice = 'Scissors';
 
    return choice;
 }
 
-console.log(getComputerChoice ());
+function getPlayerChoice() {
+    let choice = prompt('Enter your choice');
+
+    while (choice.toLowerCase() !== 'rock' | 'paper' | 'scissors') {
+        let choice = prompt('Please enter valid choice');
+}
+}
+
+let computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    let playerSelection = prompt('Enter your choice');
+    let computerSelection = getComputerChoice ();
+}
